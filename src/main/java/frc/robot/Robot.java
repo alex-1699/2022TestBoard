2db2477c3654f5b9b72d1610dc5ee75060bd5e28
Alex <96791292+alex-1699@users.noreply.github.com>
2022-02-27 16:49:53 -0500

    Cool things that I did with testing

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -26,6 +26,7 @@ import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import edu.wpi.first.networktables.NetworkTable;
@@ -41,10 +42,16 @@ public class Robot extends TimedRobot {
   public double throt;
   //drive talons
   public TalonFX m_portDriveFX1, m_portDriveFX2, m_portDriveFX3, m_starDriveFX1, m_starDriveFX2, m_starDriveFX3;
-  private DoubleSolenoid intakeSolenoid, hopperStopper, shooterAngleSolenoid, climberSolenoidPort, climberSolenoidStar;
+  private DoubleSolenoid intakeSolenoid, hopperStopper, shooterAngleSolenoid, climberSolenoidPort;
   
   public double throtSpd;
   
+  private void toggleSolenoid(final DoubleSolenoid solenoid){
+    if(solenoid.get() == DoubleSolenoid.Value.kForward){
+        solenoid.set(DoubleSolenoid.Value.kReverse);
+    }else{
+        solenoid.set(DoubleSolenoid.Value.kForward);
+    }}
 
   @Override
   public void robotInit() {
@@ -57,8 +64,8 @@ public class Robot extends TimedRobot {
 
     //drive mototrz (port)
     m_portDriveFX1 = new TalonFX(Constants.kPortDrivePort1);
-    m_portDriveFX2 = new TalonFX(Constants.kStarDrivePort2);
-    m_portDriveFX3 = new TalonFX(Constants.kStarDrivePort3);
+    m_portDriveFX2 = new TalonFX(Constants.kPortDrivePort2);
+    m_portDriveFX3 = new TalonFX(Constants.kPortDrivePort3);
     //drive mortoe (starboard)
     m_starDriveFX1 = new TalonFX(Constants.kStarDrivePort1);
     m_starDriveFX2 = new TalonFX(Constants.kStarDrivePort2);
@@ -72,7 +79,13 @@ public class Robot extends TimedRobot {
     hopperStopper = new DoubleSolenoid(Constants.kFlopperSolenoidModulePort, PneumaticsModuleType.CTREPCM, Constants.kFlopperSolenoidForwardPort, Constants.kFlopperSolenoidReversePort);
     shooterAngleSolenoid = new DoubleSolenoid(Constants.kShooterAngleSolenoidModulePort, PneumaticsModuleType.CTREPCM, Constants.kShooterAngleSolenoidForwardPort, Constants.kShooterAngleSolenoidReversePort);
     climberSolenoidPort = new DoubleSolenoid(Constants.kPortClimberModulePort, PneumaticsModuleType.CTREPCM, Constants.kPortClimberForwardPort, Constants.kPortClimberReversePort);
-    climberSolenoidStar = new DoubleSolenoid(Constants.kStarClimberModulePort, PneumaticsModuleType.CTREPCM, Constants.kStarClimberForwardPort, Constants.kStarClimberReversePort);
+
+
+    m_portDriveFX2.follow(m_portDriveFX1);
+    m_portDriveFX3.follow(m_portDriveFX1);
+
+    m_starDriveFX2.follow(m_starDriveFX1);
+    m_starDriveFX3.follow(m_starDriveFX1);
 
   }
 
@@ -107,25 +120,26 @@ public class Robot extends TimedRobot {
 
     //INTAKE! INTAKE! INTAKE...
     if (opJoystick.getRawButton(12)){
-      m_intakerMan.set(TalonSRXControlMode.PercentOutput, 0.50);
+      m_intakerMan.set(TalonSRXControlMode.PercentOutput, -0.5);
     } else {
       m_intakerMan.set(TalonSRXControlMode.PercentOutput, 0);
     }
     if (opJoystick.getRawButtonPressed(2)) {
-      intakeSolenoid.toggle();
+      toggleSolenoid(intakeSolenoid);
     }
     //HOPPA STOPPA
     if (opJoystick.getRawButtonPressed(1)) {
-      hopperStopper.toggle();
+      toggleSolenoid(hopperStopper);
     }
     if (opJoystick.getRawButtonReleased(1)) {
       hopperStopper.toggle();
+      toggleSolenoid(hopperStopper);
     }
 
     //run both shooter boys when u click button 11 on operator joystick
     if (opJoystick.getRawButton(11)){
-      m_shooterPort.set(TalonSRXControlMode.PercentOutput, 0.55);
-      m_shooterStar.set(TalonSRXControlMode.PercentOutput, -0.55);  
+      m_shooterPort.set(TalonSRXControlMode.PercentOutput, -0.3);
+      m_shooterStar.set(TalonSRXControlMode.PercentOutput, 0.3);  
     } else {
       m_shooterPort.set(TalonSRXControlMode.PercentOutput, 0);
       m_shooterStar.set(TalonSRXControlMode.PercentOutput, 0);
@@ -133,13 +147,12 @@ public class Robot extends TimedRobot {
     
     //climby climby
     if (driveJoystick.getRawButtonPressed(5)) {
-      climberSolenoidPort.toggle();
-      climberSolenoidStar.toggle();
+      toggleSolenoid(climberSolenoidPort);
     }
 
     //shooter angle UwU
     if (opJoystick.getRawButtonPressed(9)) {
-      shooterAngleSolenoid.toggle();
+      toggleSolenoid(shooterAngleSolenoid);
     }
 
   }
@@ -174,11 +187,7 @@ public class Robot extends TimedRobot {
 
     //actaully set the drive motors
     m_portDriveFX1.set(TalonFXControlMode.PercentOutput, portOutput * throt);
-    m_portDriveFX2.set(TalonFXControlMode.PercentOutput, portOutput * throt);
-    m_portDriveFX3.set(TalonFXControlMode.PercentOutput, portOutput * throt);
 
     m_starDriveFX1.set(TalonFXControlMode.PercentOutput, starOutput * throt);
-    m_starDriveFX2.set(TalonFXControlMode.PercentOutput, starOutput * throt);
-    m_starDriveFX3.set(TalonFXControlMode.PercentOutput, starOutput * throt);
 }
 }
